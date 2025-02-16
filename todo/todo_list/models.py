2fe092bd79b449476c56c173b5590ad4d6c21054
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-02-16 13:14:46 +0200

    Change max_length to 127

--- todo/todo_list/models.py
+++ todo/todo_list/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=127)
 
     def __str__(self) -> str:
         return self.name
